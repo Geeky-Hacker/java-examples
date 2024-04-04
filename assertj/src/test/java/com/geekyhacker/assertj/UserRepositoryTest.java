06a1abd114d18fc7a36e2b1ad20ea810223e83b4
kasramp <kasra_mp@yahoo.com>
2024-04-04 23:38:25 +0200

    #2 add assertj module

---
+++ assertj/src/test/java/com/geekyhacker/assertj/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.geekyhacker.assertj;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserRepositoryTest {
+
+    @InjectMocks
+    private UserRepository userRepository;
+
+    @Test
+    void shouldReturnUserByUsername() throws UserNotFoundException {
+        var expected = new User("test", "first name", "last name");
+
+        var result = userRepository.findByUsername("test");
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldThrowUserNotFoundException() {
+        assertThatThrownBy(() -> userRepository.findByUsername("nonExistenceUsername"))
+            .isInstanceOf(UserNotFoundException.class)
+            .hasMessage("Cannot find username: nonExistenceUsername");
+    }
+}
